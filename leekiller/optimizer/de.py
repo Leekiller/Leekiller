09f691efdcb494c3e1bb37d588a14a7cca46eb19
tzuhsuanchangX <tzuhsuanchang@proton.me>
2023-07-25 22:27:15 +0800

    Clean run_optimize_mutant.py

--- leekiller/optimizer/de.py
+++ leekiller/optimizer/de.py
@@ -1,41 +1,38 @@
-import os
-import glob
-import time
-import numpy as np
 from multiprocessing import Pool
+import numpy as np
 
 class DE:
-    def __init__(self, objective, control_params, control_params_range, mu=0.5, xi=0.9, c=15):
+    def __init__(
+            self, 
+            control_params: dict=None, 
+            control_params_range: dict=None, 
+            mu: float=0.5, 
+            xi: float=0.9, 
+            c: float=15
+    ):
         # DE parameters
-        self.objective = objective
         self.mu = mu    # Mutation factor
         self.xi = xi    # Crossover rate
         self.c = c
+        # Control parameters
+        if control_params is not None:
+            self.control_params = control_params
+        if control_params_range is not None:
+            self.control_params_range = control_params_range
+        # Generate populations
         self.k = 0  # Number of control parameters
-        for key in control_params:
-            self.k += len(control_params[key])  
+        for key in self.control_params:
+            self.k += len(self.control_params[key])  
         self.number_of_populations = self.c * self.k
         self.populations = []
-        # Control parameters
-        self.control_params = control_params
-        self.control_params_range = control_params_range
-        # Log data 
+        self._create_populations()
+        # Log
         self.log_objective = [[]] * self.number_of_populations
         self.log_control_params = [[]] * self.number_of_populations
         self.log_op_objective = 0
         self.log_op_control_param = []
 
-    def create_populations(self):
-        for i in range(self.number_of_populations):
-            population = {}
-            for key in self.control_params:
-                n_control_parameters = len(self.control_params[key])
-                population[key] = np.array(np.random.uniform(self.control_params_range[key][0], 
-                                                             self.control_params_range[key][1],
-                                                             n_control_parameters)).astype(int)
-            self.populations.append(population)
-
-    def run(self, itr=60):
+    def run(self, itr: int=60, batch: int=8):
         # itr: iteration times
         if len(self.populations) == 0:
             """ To do
@@ -48,19 +45,12 @@ class DE:
             print("Start differential evolution...")
             print("Number of control parameters: %s" %self.k)
             print("Populations size: %s" %len(self.populations))
+            print("itr: %s, batch: %s" %(itr, batch))
             print("==============================")
             while i < itr:
+                sampled_index = self._sample(batch)
                 with Pool() as pool:
-                    """
-                    update, update_vector, update_objective = zip(*pool.map(self._iteration,
-                                                                            range(self.number_of_populations)))
-                    """
-                    # update, update_vector, update_objective = zip(*pool.map(self._iteration, range(1)))
-                    test_i = self._sample(1)
-                    update, update_vector, update_objective = self._iteration(test_i[0])
-                    update = [update]
-                    update_vector = [update_vector]
-                    update_objective = [update_objective]
+                    update, update_vector, update_objective = zip(*pool.map(self._iteration, sampled_index))
                 for j in range(len(update)):
                     self.log_objective[j].append(update_objective[j])
                     self.log_control_params[j].append(update_vector[j])
@@ -75,17 +65,31 @@ class DE:
                          objective=self.log_objective, 
                          control_params=self.log_control_params,
                          op_objective=self.log_op_objective)
-                #i = i + self.number_of_populations
-                i = i + 1
+                i = i + batch
             print('Finish %s iterations, optimized ROI: %.6f%%' %(i, self.log_op_objective))
             print('==============================')            
 
-    def _objective(self):
-        """ Objective function
+    def load_data(self, data_path: str=None):
+        """ Load market data to self.dataframe
 
-        Using ROI
         """
-        return None
+        raise NotImplementedError("No market data!")
+
+    def get_objective_value(self, control_params: dict) -> float:
+        raise NotImplementedError("Objective value not defined!")
+
+    def _create_populations(self):
+        for i in range(self.number_of_populations):
+            population = {}
+            for key in self.control_params:
+                if key in self.control_params_range:
+                    n_control_parameters = len(self.control_params[key])
+                    population[key] = np.array(np.random.uniform(self.control_params_range[key][0], 
+                                                                 self.control_params_range[key][1],
+                                                                 n_control_parameters)).astype(int)
+                elif (key in self.control_params) and (key not in self.control_params_range):
+                    population[key] = self.control_params[key]
+            self.populations.append(population)
 
     def _sample(self, n, not_include=[]):
         # Randomly sample n populations for one differential evolution iteration
@@ -121,20 +125,23 @@ class DE:
             di1_vector = self.populations[dix_index[0]]
             di2_vector = self.populations[dix_index[1]]
             di3_vector = self.populations[dix_index[2]]
-            for key in di1_vector:
-                mi_vector[key] = (np.array(di1_vector[key]) +
-                                  self.mu*(np.array(di2_vector[key])-np.array(di3_vector[key]))
-                                  ).astype(int)
-                # Check the validation of mi_vector
-                invalid = [temp for temp in mi_vector[key] 
-                           if temp < self.control_params_range[key][0] 
-                           or temp > self.control_params_range[key][1]
-                           ]
-                if len(invalid) != 0:
-                    valid_mutation = False
-                    break
+            for key in self.control_params:
+                if key in self.control_params_range:
+                    mi_vector[key] = (np.array(di1_vector[key]) +
+                                      self.mu*(np.array(di2_vector[key])-np.array(di3_vector[key]))
+                                      ).astype(int)
+                    # Check the validation of mi_vector
+                    invalid = [temp for temp in mi_vector[key] 
+                               if temp < self.control_params_range[key][0] 
+                               or temp > self.control_params_range[key][1]
+                               ]
+                    if len(invalid) != 0:
+                        valid_mutation = False
+                        break
+                    else:
+                        valid_mutation = True
                 else:
-                    valid_mutation = True
+                    mi_vector[key] = di1_vector[key]
         return mi_vector
     
     def _crossover(self, di_index: int, mi_vector: dict):
@@ -143,14 +150,17 @@ class DE:
         """
         di_vector = self.populations[di_index]
         ci_vector = {}  # Target vector
-        for key in di_vector:
-            ci_vector[key] = []
-            for i in range(len(di_vector[key])):
-                r = np.random.uniform(0, 1)
-                if r < self.xi:
-                    ci_vector[key].append(mi_vector[key][i])
-                else:
-                    ci_vector[key].append(di_vector[key][i])
+        for key in self.control_params:
+            if key in self.control_params_range:
+                ci_vector[key] = []
+                for i in range(len(di_vector[key])):
+                    r = np.random.uniform(0, 1)
+                    if r < self.xi:
+                        ci_vector[key].append(mi_vector[key][i])
+                    else:
+                        ci_vector[key].append(di_vector[key][i])
+            else:
+                ci_vector[key] = di_vector[key]
         return ci_vector
 
     def _selection(self, di_index, ci_vector: dict):
@@ -158,8 +168,8 @@ class DE:
 
         """
         di_vector = self.populations[di_index]
-        di_objective = self.objective(di_vector)
-        ci_objective = self.objective(ci_vector)
+        di_objective = self.get_objective_value(di_vector)
+        ci_objective = self.get_objective_value(ci_vector)
         update = False
         update_vector = None
         update_objective = None
